b69c8f8c7d3d5533a93e6f13534d9890ec8e0e10
anlideer <anlideer@gmail.com>
2023-10-29 14:54:59 +0200

    add color tag support for typer

--- README.md
+++ README.md
@@ -6,4 +6,4 @@
 Follow mouse (updating length and rotation). Or set start and end position in the codes (not the attributes start&end point) and let it updating dynamically. Or set two squares (in the attributes) and let it updating dynamically (starting and ending at the border of the squares). (Maybe need to modify some, depending on the size of the square.)
 
 ## Typing
-Very very simple and naive implementation of a character-by-character text typing script.
+Very very simple and naive implementation of a character-by-character text typing script. Support color tag that is supported by TextMeshPro.
--- Typing/TypingWriter.cs
+++ Typing/TypingWriter.cs
@@ -2,20 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System.Text.RegularExpressions;
 
 // attached to the element with TextMeshProUGUI component
 public class TypingWriter : MonoBehaviour
 {
-    [SerializeField] private float characterDuration = .5f;
+    [SerializeField] private float characterDuration = .1f;
+    [SerializeField] private float textAliveDuration = 6f;
 
     private TextMeshProUGUI textMesh;
     private string textBuffer = "";
     private float lastPushTime;
+    private readonly object bufferLock = new object();
+    private readonly object displayedTextLock = new object();
 
     private void Awake()
     {
         textMesh = GetComponent<TextMeshProUGUI>();
-        if (textMesh == null ) 
+        if (textMesh == null) 
         {
             Debug.LogWarning("TypingWriter won't work without TextMeshProUGUI component");
         }
@@ -27,17 +31,65 @@ public class TypingWriter : MonoBehaviour
 
     private void Update()
     {
-        if (textBuffer.Length > 0 && Time.timeSinceLevelLoad - lastPushTime >= characterDuration)
+        lock (bufferLock)
         {
-            lastPushTime = Time.timeSinceLevelLoad;
-            char c = textBuffer[0];
-            textMesh.text = textMesh.text + c;
-            textBuffer = textBuffer.Substring(1);
+            while (textBuffer.Length > 0 && Time.timeSinceLevelLoad - lastPushTime >= characterDuration)
+            {
+                lastPushTime = Time.timeSinceLevelLoad;
+                char c = textBuffer[0];
+
+                // support html tag
+                if (c == '<')
+                {
+                    string tag = "<";
+                    int i = 1;
+                    while (i < textBuffer.Length && textBuffer[i] != '>')
+                    {
+                        tag += textBuffer[i];
+                        i++;
+                    }
+                    // add one more character
+                    if (i < textBuffer.Length)
+                    {
+                        tag += textBuffer[i];
+                    }
+                    lock (displayedTextLock)
+                    {
+                        textMesh.text = textMesh.text + tag;
+                        textBuffer = textBuffer.Substring(i+1);
+                    }
+                }
+                else
+                {
+                    lock (displayedTextLock)
+                    {
+                        textMesh.text = textMesh.text + c;
+                        textBuffer = textBuffer.Substring(1);
+                    }
+                }
+            }
         }
     }
 
     public void PushText(string text)
     {
-        textBuffer += text;
+        lock (bufferLock)
+        {
+            textBuffer += text;
+            StartCoroutine(EraseString(text, textAliveDuration));
+        }
+    }
+
+    private IEnumerator EraseString(string str, float aliveDuration)
+    {
+        yield return new WaitForSeconds(aliveDuration);
+        lock (displayedTextLock)
+        {
+            if (textMesh.text.StartsWith(str))
+            {
+                int len = str.Length;
+                textMesh.text = textMesh.text.Substring(len);
+            }
+        }
     }
 }
